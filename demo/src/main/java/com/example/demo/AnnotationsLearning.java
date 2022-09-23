18d279c3dfe3bde3276ee5276aba8696d1ee6a17
maldanna <maldanna432@gmail.com>
2022-09-23 18:40:18 +0000

    second commit

---
+++ demo/src/main/java/com/example/demo/AnnotationsLearning.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+public class AnnotationsLearning {
+    private Integer cost;  
+
+    public void setCost(Integer cost)   
+    {  
+    this.cost = cost;  
+    }  
+    public Integer getCost()   
+    {  
+    return cost;  
+    }   
+
+
+}
--- demo/src/main/java/com/example/demo/controller/UserRestController.java
+++ demo/src/main/java/com/example/demo/controller/UserRestController.java
@@ -1,8 +1,36 @@
 package com.example.demo.controller;
 
+import javax.websocket.server.PathParam;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/demo")
 public class UserRestController {
+    @GetMapping("/login/{id}")
+    public String getMethod(@PathParam("id") String userName){
+        return "get test";
+    }
+
+    @PostMapping("/signup")
+    public String postMethod(@RequestBody User user){
+        return "post test";
+    }
+
+    @PutMapping("/put")
+    public String putMethod(){
+        return "post test";
+    }
+
+    @DeleteMapping("/delete")
+    public String deleteMethod(){
+        return "post test";
+    }
+
+
     
 }
