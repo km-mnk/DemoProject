25c4aa30d09c36c20d63316c4f739651ea11ea9a
maldanna <maldanna432@gmail.com>
2022-09-25 18:03:15 +0000

    sprig boot AOP concepts and logging sfj4

---
+++ demo/src/main/java/com/example/demo/aop/CentralisedLogging.java
@@ -0,0 +1,49 @@
+package com.example.demo.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+//import org.aspectj.lang.annotation.Aspect;
+
+
+@Aspect
+@Component
+public class CentralisedLogging {
+
+	@Before(value = "execution(* com.example.demo.controller.UserRestController.*(..))")
+	public void beforeAdvice(JoinPoint joinPoint) {
+        System.out.println("before advice");
+        //System.out.println("class name is:" +joinPoint.getClass().getName());
+		System.out.println("Before method:" + joinPoint.getSignature());
+       // System.out.println("args names are : "+joinPoint.getArgs().toString());
+
+	}
+
+	@After(value = "execution(* com.example.demo.controller.UserRestController.*(..))")
+	public void afterAdvice(JoinPoint joinPoint) {
+        System.out.println("after advice");
+
+	}
+
+    
+	@AfterReturning(pointcut = "execution(* com.example.demo.controller.UserRestController.*(..))",returning = "result")
+	public void afterReturning(JoinPoint joinPoint,Object result) {
+        System.out.println("afterReturning advice");
+        System.out.println("return values is :" + result.toString());
+
+	}
+
+   /*  @Around(value = "execution(* com.example.demo.controller.*.*(..))")
+	public void aroundAdvice(ProceedingJoinPoint pjp) {
+        System.out.println("after advice");
+        System.out.println("class name is:" +pjp.getClass().getName());
+		System.out.println("Before method:" + pjp.getSignature());
+        System.out.println("args names are : "+pjp.getArgs().toString());
+	}*/
+
+
+}
